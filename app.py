2c7c6a86483c807340f5c20add468ca2dc8e787b
ingmauriciogarcia <mauricio.garcia.uped@gmail.com>
2025-05-19 19:20:12 -0600

    Version3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola desde Azure con Entrega Continua Version2!"
+    return "¡Hola desde Azure con Entrega Continua Version3!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
